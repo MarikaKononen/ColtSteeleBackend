1497b8ea27eb7e22c3709ef3351df801d3a79610
MarikaKononen <marika.kononen@koodielamastani.fi>
2019-07-30 21:40:20 +0300

    Refactored Express exercise

--- IntroToExpress/app.js
+++ IntroToExpress/app.js
@@ -8,53 +8,41 @@ let app = express();
  });
 
  // simple Routes
- app.get('/mom/clean', function(req, res){
-    res.send("The mom cleans the house");
+ app.get('/mom/:momdothis', function(req, res){
+  
+   let dothis = {
+      clean: "The mom cleans the house",
+      food: "The mom makes the food", 
+      goods: "The mom finds the goods",
+      everything: "The mom do everything"
+   }
+   let momdothis = req.params.momdothis.toLowerCase();
+   let task = dothis[momdothis];
+   res.send(task); 
+
  });
 
- app.get('/mom/food', function(req, res){
-    res.send("The mom makes the food");
- });
-
- app.get('/mom/goods', function(req, res){
-    res.send("The mom finds the goods");
- });
 
  // Route patterns
- app.get('/mom/vacuum/:number', function(req, res){
-    let numberOfCleaning = parseInt(req.params.number);
+ app.get('/mom/:task/:number', function(req, res){
+    let number = Number(req.params.number);
+    let task = req.params.task;
     let str = '';
 
-    for( i = 0; i < numberOfCleaning; i++ ){
-       str += "vacuum "
+    for( i = 0; i < number; i++ ){
+       str += task;
+       str += " ";
        console.log(i);
        console.log(str);
     }
-    res.send("The mom vacuums the house " + numberOfCleaning + " times in this week " + "<br> <br>" + str )
+    res.send("The mom do " +   task + " " + number + " times in this week " + "<br> <br>" + str )
 
  });
-
- app.get('/mom/laundry/:number', function(req, res){
-   let numberOfCleaning = parseInt(req.params.number);
-   let str = '';
-
-   for( i = 0; i < numberOfCleaning; i++ ){
-      str += "laundry "
-      console.log(i);
-      console.log(str);
-   }
-   res.send("The mom do laundry " + numberOfCleaning + " times in this week " + "<br> <br>" + str )
-
-});
 
 app.get('*', function(req, res){
    res.send("Sorry, mom is tired please contact tomorrow again!" )
 
 });
-
-
-
-
 
  // Tell Express to listen for requests (start server)
 app.listen(3000, function(){
