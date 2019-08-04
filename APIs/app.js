a071bc0a8fafa060da05ceac19580fbe0d7f04bd
MarikaKononen <marika.kononen@koodielamastani.fi>
2019-08-04 15:59:49 +0300

    Added API exercise

---
+++ APIs/app.js
@@ -0,0 +1,54 @@
+let express = require("express");
+let app = express();
+let bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+let dogsUrls = [];
+let breedName = "";
+
+const requestBreedImg = (breed) => {
+    let request = require('request');
+    let urlBegin = 'https://dog.ceo/api/breed/';
+    let urlEnd = '/images';
+    let url = urlBegin + breed + urlEnd;
+    console.log('url', url);
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            // check type of the body variable
+            console.log(typeof body);
+            // turn body to javascript 
+            let parsedData = JSON.parse(body);
+            console.log('length: ', parsedData["message"].length);
+            for(i=0; i < parsedData["message"].length; i++){
+                dogsUrls.push(parsedData["message"][i]);
+            }
+            console.log("requestBreedImg function executed");
+        }
+    });
+}
+
+app.get("/", function(req, res){
+    res.render("home");
+    dogsUrls = [];
+});
+
+app.get("/dogs", function(req, res){
+    res.render("dogs", {dogs: dogsUrls, breed: breedName});
+});
+
+// post route for creating a new friend
+app.post("/addbreed", function(req, res){
+    console.log("addbreed", req.body)
+    let newBreed = req.body.newbreed.toLowerCase();
+    breedName = newBreed;
+    console.log('breed', newBreed);
+    requestBreedImg(newBreed);
+    res.redirect("/dogs");
+});
+
+
+app.listen(3000, function(){
+    console.log("Server started");
+});
